7c20d357e0f3f75b55c1d46797eb22a2994200e8
Josep Piguillem <pigui_prat94@hotmail.es>
2018-08-20 15:02:43 +0200

    AuthController

---
+++ server/app/Http/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Validator;
+use Auth;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+}
